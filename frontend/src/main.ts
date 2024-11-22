6eed74d2f4ba0ea1dcbbe66b2c3f409ef0864ef4
Aude <aude@compani.fr>
2024-11-22 17:15:05 +0100

    forgot to save main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import router from './Router'
+import store from '../src/store/index'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.mount('#app');
